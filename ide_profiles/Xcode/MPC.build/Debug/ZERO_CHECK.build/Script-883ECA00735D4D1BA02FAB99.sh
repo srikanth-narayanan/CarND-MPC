53e0a1369dff20804605f2b923d07389d9b5ab83
Srikanth Narayanan <srikanthnarayanan@Srikanths-MacBook-Pro.local>
2017-10-15 18:58:07 -0700

    built main to solve on mpc. added display lines

--- ide_profiles/Xcode/MPC.build/Debug/ZERO_CHECK.build/Script-883ECA00735D4D1BA02FAB99.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/srikanthnarayanan/Documents/GitHubRepos/CarND-MPC/ide_profiles/Xcode -f /Users/srikanthnarayanan/Documents/GitHubRepos/CarND-MPC/ide_profiles/Xcode/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
--- src/main.cpp
+++ src/main.cpp
@@ -122,14 +122,19 @@ int main() {
           double cte = polyeval(coeffs, 0); // Assumption for initial point x=0, y=0
           double epsi = -atan(coeffs[1]);
           
+          cout << coeffs << endl;
+          cout << cte << endl;
+          cout << epsi << endl;
+          
           // Setup initial state
           Eigen::VectorXd state(6);
           state << 0, 0, 0, v, cte, epsi;
           
           // use mpc to solve
           auto vars = mpc.Solve(state, coeffs);
-          
-          
+          steer_value = vars[0];
+          throttle_value = vars[1];
+        
 
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
@@ -143,6 +148,14 @@ int main() {
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
+          for(int i = 2; i < vars.size(); i++){
+            if(i % 2 == 0){
+              mpc_x_vals.push_back(vars[i]);
+            }
+            else{
+              mpc_y_vals.push_back(vars[i]);
+            }
+          }
 
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
@@ -153,6 +166,11 @@ int main() {
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
+          
+          for(int i = 0; i < 75; i++){
+            next_x_vals.push_back(i);
+            next_y_vals.push_back(polyeval(coeffs, i));
+          }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
